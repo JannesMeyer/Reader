1efd4fb300b32d3a2066410261df897ba38f55cb
Jannes <jannes.meyer@gmail.com>
2012-06-21 18:29:30 +0200

    Fix javascript errrors

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,29 +1,36 @@
 $(function() {
+  var articleNode = document.getElementById('articleSwipe');
+  var tabNode = document.getElementById('tabs');
+
   // articleSwipe
-    new Swipe(document.getElementById('articleSwipe'));
-
-  // tabs
-      tabs = new Swipe(document.getElementById('tabs'), {
-        callback: function(event,index,elem) {
-          setTab(selectors[index]);
-        }
-      }),
-      selectors = document.getElementById('tabSelector').children;
-
-  for (var i = 0; i < selectors.length; i++) {
-    var elem = selectors[i];
-    elem.setAttribute('data-tab', i);
-    elem.onclick = function(e) {
-      e.preventDefault();
-      setTab(this);
-      tabs.slide(parseInt(this.getAttribute('data-tab'),10),300);
+  if (articleNode) {
+    new Swipe(articleNode);
+  }
+  // 
+  if (tabNode) {
+    // Set tab helper function
+    function setTab(elem) {
+      for (var i = 0; i < selectors.length; i++) {
+        selectors[i].className = selectors[i].className.replace('on',' ');
+      }
+      elem.className += ' on';
     }
+
+    var tabs = new Swipe(tabNode, {
+      callback: function(event,index,elem) {
+        setTab(selectors[index]);
+      }
+    });
+    var selectors = document.getElementById('tabSelector').children;
+
+    for (var i = 0; i < selectors.length; i++) {
+      var elem = selectors[i];
+      elem.setAttribute('data-tab', i);
+      elem.onclick = function(e) {
+        e.preventDefault();
+        setTab(this);
+        tabs.slide(parseInt(this.getAttribute('data-tab'),10),300);
+      }
+    } 
   }
 });
-
-function setTab(elem) {
-  for (var i = 0; i < selectors.length; i++) {
-    selectors[i].className = selectors[i].className.replace('on',' ');
-  }
-  elem.className += ' on';
-}
