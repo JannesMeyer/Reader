c03c915ddb06371f03c0f64962eb2ef19626a33c
Jannes <jannes.meyer@gmail.com>
2012-07-12 16:20:51 +0200

    Implement the login

--- routes/index.js
+++ routes/index.js
@@ -1,102 +1,174 @@
-module.exports = function(app, db) {
+var crypto = require('crypto');
+
+function sha1(str) {
+	var shasum = crypto.createHash('sha1');
+	shasum.update(str);
+	return shasum.digest('hex');
+}
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		res.redirect('/login');
+	}
+
+// Export a function that defines our routes
+module.exports = function(app, db, passport) {
 	/*
-	 * Index
+	 * GET /
 	 */
 	app.get('/', function(req, res) {
-		res.render('index', { title: 'Test' });
+		var data = {
+			title: 'inForm',
+			user: req.user
+		};
+		res.render('index', data);
 	});
 
 	/*
-	 * All feeds
+	 * GET /login
 	 */
-	app.get('/feeds', function(req, res) {
-		feeds = [
-			{
-				id: '1',
-				name: 'Smashing Magazine',
-				image_path: undefined
-			}
-		];
-
-		// {
-		// 	name: 'Smashing Magazine',
-		// 	image_path: '',
-		// 	url: 'http://rss1.smashingmagazine.com/feed/'
-		// }
-
-		res.render('all_feeds', { title: 'Feeds' });
+	app.get('/login',	function(req, res) {
+		var data = {
+			title: 'Login',
+			user: req.user,
+			message: req.flash('error')
+		};
+		res.render('login', data);
 	});
 
 	/*
-	 * Add feed
+	 * POST /login
 	 */
-	app.get('/add-feed', function(req, res) {
-		res.render('add_feed', { title: 'Feed hinzufügen' });
+	app.post('/login',
+		passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+		function(req, res) {
+			res.redirect('/');
+		});
+
+	/*
+	 * GET /logout
+	 */
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
 	});
 
 	/*
-	 * All articles
+	 * GET /feeds
+	 */
+	app.get('/feeds', ensureAuthenticated, function(req, res) {
+		// Get all feeds
+		db.collection('feeds', function(err, feeds) {
+			feeds.find().limit(20).toArray(function(err, feedList) {
+				console.log(feedList);
+				var data = {
+					title: 'Feeds',
+					user: req.user,
+					feeds: feedList
+				};
+				res.render('all_feeds', data);
+			});
+		});
+	});
+
+	/*
+	 * GET /add-feed
+	 */
+	app.get('/add-feed', ensureAuthenticated, function(req, res) {
+		var data = {
+			title: 'Feed hinzufügen',
+			user: req.user
+		};
+		res.render('add_feed', data);
+	});
+
+	/*
+	 * GET /feeds/:id
 	 */
 	app.get('/feeds/:id', function(req, res) {
 		var data = {
 			title: 'Artikel',
-			articles: []
-		};
-		data.articles[0] = {
-			id: 1,
-			title: "Test 1",
-			text: "DSLR anim dreamcatcher sint dumpster incididunt. Yr chillwave DSLR street-art letterpress gentrify liberal. Farm-to-table bronson organic narwhal ethical clothesline. Frado authentic gastropub art frado kale brooklyn. Placeat authentic gluten fin liberal 8-bit.",
-			time: "1234",
-			image: "/images/test.jpg"
-		};
-		data.articles[1] = {
-			id: 2,
-			title: "Test 2",
-			text: "Before 8-bit brooklyn frado trust-fund ut. Chillwave capitalism placeat vegan. Anderson moon fin totally chowder original. The sunt dumpster dumpster voluptate chowder.",
-			time: "1234",
-			image: "/images/test.jpg"
-		};
-		data.articles[2] = {
-			id: 3,
-			title: "Test 3",
-			text: "Clothesline esse sriracha gluten-free farm-to-table Pinterest. Carles street-art anim anime wes. Capitalism vegan gluten-free farm-to-table wayfarers gluten-free. Sint organic chowder street-art 8-bit anim of party. Narwhal of fresh twee. Viral vegan Anderson pony Anderson.",
-			time: "1234",
-			image: "/images/test.jpg"
-		};
+			user: req.user,
+			articles: [
 
+			]
+		};
 		// Render the template
 		res.render('all_articles', data);
 	});
 
 	/*
-	 * Article
+	 * GET /articles/:id
 	 */
 	app.get('/articles/:id', function(req, res) {
-		res.render('article', { title: 'Artikel' });
+		var data = {
+			title: 'Artikel',
+			user: req.user
+		};
+		res.render('article', data);
 	});
 
 
-	// Reset db
+	/*
+	 * GET /reset_db
+	 */
 	app.get('/reset_db', function(req, res) {
 		db.dropDatabase(function(err, result) {
-			db.collection('users', function(err, userCollection) {
-				//userCollection.insert([]);
-				userCollection.save({
+
+			db.collection('feeds', function(err, feeds) {
+				db.collection('articles', function(err, articles) {
+					articles.insert([{
+							title: 'Test 1',
+							text: 'DSLR anim dreamcatcher sint dumpster incididunt. Yr chillwave DSLR street-art letterpress gentrify liberal. Farm-to-table bronson organic narwhal ethical clothesline. Frado authentic gastropub art frado kale brooklyn. Placeat authentic gluten fin liberal 8-bit.',
+							published_at: new Date(2012, 2, 31),
+							image: '/images/test.jpg'
+						}, {
+							title: 'Test 2',
+							text: 'Before 8-bit brooklyn frado trust-fund ut. Chillwave capitalism placeat vegan. Anderson moon fin totally chowder original. The sunt dumpster dumpster voluptate chowder.',
+							published_at: new Date(2012, 5, 16),
+							image: '/images/test.jpg'
+						}, {
+							title: 'Test 3',
+							text: 'Clothesline esse sriracha gluten-free farm-to-table Pinterest. Carles street-art anim anime wes. Capitalism vegan gluten-free farm-to-table wayfarers gluten-free. Sint organic chowder street-art 8-bit anim of party. Narwhal of fresh twee. Viral vegan Anderson pony Anderson.',
+							published_at: new Date(2012, 3, 1),
+							image: '/images/test.jpg'
+						}]);
+					var articleList = articles.find({}, ['_id']).toArray(function(err, articleList) {
+						console.log(articleList);
+						feeds.save({
+							name: 'Smashing Magazine',
+							image_path: undefined,
+							feed_url: 'http://rss1.smashingmagazine.com/feed/',
+							articles: articleList
+						});
+					});
+				});
+			});
+			db.collection('users', function(err, users) {
+				//users.insert([]);
+				users.save({
 					email: 'jannes.meyer@gmail.com',
 					name: 'Jannes Meyer',
-					password: 'test',
+					password: sha1('test'),
 					feeds: []
 				});
-				userCollection.save({
+				users.save({
 					email: 'fuhlig@gmail.com',
 					name: 'Florian Uhlig',
-					password: 'test',
+					password: sha1('test'),
 					feeds: []
 				});
-				userCollection.save({
+				users.save({
 					email: 'fuhlig@gmail.com',
-					name: 'Florian Uhlig',
-					password: 'test',
+					name: 'Magdalena Riecken',
+					password: sha1('test'),
 					feeds: []
 				});
 				res.end('Success');
@@ -104,8 +176,10 @@ module.exports = function(app, db) {
 		});
 	});
 
-	/*return {
+	/* CommonJS exports
+	return {
 		func : function () {}
-	}*/
+	}
 	//module.func();
+	*/
 };
