30a8be943c0bbd7ddd983b09c15e962f93286836
Jannes <jannes.meyer@gmail.com>
2012-07-18 12:15:42 +0200

    Further development of the database and reorganization of the dialog views

--- app.js
+++ app.js
@@ -4,102 +4,69 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var crypto = require('crypto');
 var LessMiddleware = require('less-middleware');
-var Db = require('mongodb').Db;
-var Connection = require('mongodb').Connection;
-var Server = require('mongodb').Server;
+var mongo = require('mongoskin');
 var ObjectID = require('mongodb').ObjectID;
 
 // Start server
 initializeDatabase();
-
-// Authentication helpers
-var users = [
-		{ id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-	, { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-	var idx = id - 1;
-	if (users[idx]) {
-		fn(null, users[idx]);
-	} else {
-		fn(new Error('User ' + id + ' does not exist'));
-	}
-}
-
-function findByUsername(username, fn) {
-	for (var i = 0, len = users.length; i < len; i++) {
-		var user = users[i];
-		if (user.username === username) {
-			return fn(null, user);
-		}
-	}
-	return fn(null, null);
-}
-
-function sha1(str) {
-	var shasum = crypto.createHash('sha1');
-	shasum.update(str);
-	return shasum.digest('hex');
-}
 
 // Create a MongoDB connection, if successful continue with
 function initializeDatabase() {
 	//var FeedProvider = require('./providers/feed_provider');
 
 	// Persistence
-	var client = new Db('reader', new Server('127.0.0.1', 27017, {}));
-	client.open(function(err, db) {
-		console.log('Connected to MongoDB');
+	var db = mongo.db('127.0.0.1:27017/reader?auto_reconnect');
+	db.bind('users');
 
-		// Authentication
-		db.collection('users', function(err, users) {
-			if (err) {
-				console.log('Couldn\'t find the users collection');
-				return;
-			}
-
-			// Setup user object serialiation/deserialization
-			passport.serializeUser(function(user, done) {
-				done(null, user._id);
-			});
-			passport.deserializeUser(function(id, done) {
-				users.findOne({_id: new ObjectID(id)}, function(err, user) {
-					done(err, user);
-				});
-			});
-
-			// Use the LocalStrategy within Passport
-			function verifyLogin(username, password, done) {
-				console.log('Trying to login as %s (password: %s)', username, password);
-
-				// Find the user by username.  If there is no user with the given
-				// username, or the password is not correct, set the user to `false` to
-				// indicate failure and set a flash message.  Otherwise, return the
-				// authenticated `user`.
-				users.findOne({email: username}, function(err, user) {
-					if (err) {
-						// Database error
-						return done(err);
-					}
-					if (!user) {
-						// User not found
-						return done(null, false, { message: 'Unknown user ' + username });
-					}
-					if (user.password != sha1(password)) {
-						// Password incorrect
-						return done(null, false, { message: 'Invalid password' });
-					}
-					// Success!
-					return done(null, user);
-				});
-			}
-			passport.use(new LocalStrategy(verifyLogin));
-
-			// Initialize Express.js
-			initializeHttpServer(db); // continue here
-		});
+	// Setup user object serialiation/deserialization
+	passport.serializeUser(function(user, done) {
+		done(null, user._id);
 	});
+	passport.deserializeUser(function(id, done) {
+		db.users.findById(id, function(err, user) {
+			done(err, user);
+		});
+		/*db.users.findOne({_id: new ObjectID(id)}, function(err, user) {
+			done(err, user);
+		});*/
+	});
+
+	// Authentication helper
+	function checkPassword(user, password) {
+		var shasum = crypto.createHash('sha1');
+		shasum.update(password);
+		var hashedPassword = shasum.digest('hex');
+		return user.password === hashedPassword;
+	}
+
+	// Use the LocalStrategy within Passport
+	function verifyLogin(username, password, done) {
+		console.log('Trying to login as %s (password: %s)', username, password);
+
+		// Find the user by username.  If there is no user with the given
+		// username, or the password is not correct, set the user to `false` to
+		// indicate failure and set a flash message.  Otherwise, return the
+		// authenticated `user`.
+		db.users.findOne({email: username}, function(err, user) {
+			if (err) {
+				// Database error
+				return done(err);
+			}
+			if (!user) {
+				// User not found
+				return done(null, false, { message: 'Unknown user ' + username });
+			}
+			if (!checkPassword(user, password)) {
+				// Password incorrect
+				return done(null, false, { message: 'Invalid password' });
+			}
+			// Success!
+			return done(null, user);
+		});
+	}
+	passport.use(new LocalStrategy(verifyLogin));
+
+	initializeHttpServer(db); // continue here
 }
 
 function initializeHttpServer(db) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto');
+var url = require('url');
 
 function sha1(str) {
 	var shasum = crypto.createHash('sha1');
@@ -12,31 +13,50 @@ function sha1(str) {
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		}
-		res.redirect('/login');
+	if (req.isAuthenticated()) {
+		return next();
 	}
+	res.redirect('/login');
+}
 
 // Export a function that defines our routes
 module.exports = function(app, db, passport) {
+	var ObjectId = db.ObjectID;
+	db.bind('users');
+	db.bind('feeds', {
+		findByUrl: function(feedUrl, callback) {
+			this.findOne({feed_url: url.format(url.parse(feedUrl))}, callback);
+		}
+	});
+	db.bind('articles');
+	
+
 	/*
 	 * GET /
 	 */
 	app.get('/', function(req, res) {
 		var data = {
-			title: 'inForm',
+			title: 'Feeds',
 			user: req.user
 		};
+		if (req.user) {
+		// Logged in; get all feeds
+		db.feeds.find({subscribers: req.user._id}).toArray(function(err, feeds) {
+			data.feeds = feeds;
+			res.render('all_feeds', data);
+		});
+		} else {
+		// Not logged in; show different page
 		res.render('index', data);
+		}
 	});
 
 	/*
 	 * GET /login
 	 */
-	app.get('/login',	function(req, res) {
+	app.get('/login', function(req, res) {
 		var data = {
-			title: 'Login',
+			title: 'Einloggen',
 			user: req.user,
 			message: req.flash('error')
 		};
@@ -46,11 +66,29 @@ module.exports = function(app, db, passport) {
 	/*
 	 * POST /login
 	 */
-	app.post('/login',
-		passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-		function(req, res) {
-			res.redirect('/');
-		});
+	var loginAuth = passport.authenticate('local', { failureRedirect: '/login', failureFlash: true });
+	app.post('/login', loginAuth, function(req, res) {
+		res.redirect('/');
+	});
+
+	/*
+	 * GET /register
+	 */
+	app.get('/register', function(req, res) {
+		var data = {
+			title: 'Registrieren',
+			user: req.user,
+			message: req.flash('error')
+		};
+		res.render('register', data);
+	});
+
+	/*
+	 * GET /register
+	 */
+	app.get('/register', function(req, res) {
+		console.log('register');
+	});
 
 	/*
 	 * GET /logout
@@ -58,24 +96,6 @@ module.exports = function(app, db, passport) {
 	app.get('/logout', function(req, res) {
 		req.logout();
 		res.redirect('/');
-	});
-
-	/*
-	 * GET /feeds
-	 */
-	app.get('/feeds', ensureAuthenticated, function(req, res) {
-		// Get all feeds
-		db.collection('feeds', function(err, feeds) {
-			feeds.find().limit(20).toArray(function(err, feedList) {
-				console.log(feedList);
-				var data = {
-					title: 'Feeds',
-					user: req.user,
-					feeds: feedList
-				};
-				res.render('all_feeds', data);
-			});
-		});
 	});
 
 	/*
@@ -90,18 +110,69 @@ module.exports = function(app, db, passport) {
 	});
 
 	/*
+	 * POST /add-feed
+	 */
+	app.post('/add-feed', ensureAuthenticated, function(req, res) {
+		var params = req.body;
+		// Add feed
+		db.feeds.findByUrl(params.url, function(err, feed) {
+			if (!feed) {
+				// TODO: check if the URL is a valid feed
+				// Create feed
+				feed = {
+					name: params.name,
+					feed_url: url.format(url.parse(params.url)),
+					subscribers: [req.user._id]
+				};
+				db.feeds.save(feed);
+
+				// Create test articles
+				var articles = [
+					{
+						title: 'Test 1',
+						text: 'DSLR anim dreamcatcher sint dumpster incididunt. Yr chillwave DSLR street-art letterpress gentrify liberal. Farm-to-table bronson organic narwhal ethical clothesline. Frado authentic gastropub art frado kale brooklyn. Placeat authentic gluten fin liberal 8-bit.',
+						published_at: new Date(2012, 2, 31),
+						image: '/images/test.jpg',
+						feed: feed._id
+					}, {
+						title: 'Test 2',
+						text: 'Before 8-bit brooklyn frado trust-fund ut. Chillwave capitalism placeat vegan. Anderson moon fin totally chowder original. The sunt dumpster dumpster voluptate chowder.',
+						published_at: new Date(2012, 5, 16),
+						image: '/images/test.jpg',
+						feed: feed._id
+					}, {
+						title: 'Test 3',
+						text: 'Clothesline esse sriracha gluten-free farm-to-table Pinterest. Carles street-art anim anime wes. Capitalism vegan gluten-free farm-to-table wayfarers gluten-free. Sint organic chowder street-art 8-bit anim of party. Narwhal of fresh twee. Viral vegan Anderson pony Anderson.',
+						published_at: new Date(2012, 3, 1),
+						image: '/images/test.jpg',
+						feed: feed._id
+					}
+				];
+				db.articles.insert(articles);
+			} else {
+				// TODO: avoid subscribing the same user twice
+				db.feeds.update({ _id: feed._id }, { '$push': { subscribers: req.user._id } });
+			}
+		});
+
+		//TODO: AJAX
+		res.redirect('/');
+	});
+
+	/*
 	 * GET /feeds/:id
 	 */
 	app.get('/feeds/:id', function(req, res) {
 		var data = {
 			title: 'Artikel',
-			user: req.user,
-			articles: [
-
-			]
+			user: req.user
 		};
-		// Render the template
-		res.render('all_articles', data);
+		db.articles.find({feed: req.params.id}).toArray(function(err, articles) {
+			data.articles = articles;
+			console.log(articles);
+			// Render the template
+			res.render('all_articles', data);
+		});
 	});
 
 	/*
@@ -121,58 +192,21 @@ module.exports = function(app, db, passport) {
 	 */
 	app.get('/reset_db', function(req, res) {
 		db.dropDatabase(function(err, result) {
-
-			db.collection('feeds', function(err, feeds) {
-				db.collection('articles', function(err, articles) {
-					articles.insert([{
-							title: 'Test 1',
-							text: 'DSLR anim dreamcatcher sint dumpster incididunt. Yr chillwave DSLR street-art letterpress gentrify liberal. Farm-to-table bronson organic narwhal ethical clothesline. Frado authentic gastropub art frado kale brooklyn. Placeat authentic gluten fin liberal 8-bit.',
-							published_at: new Date(2012, 2, 31),
-							image: '/images/test.jpg'
-						}, {
-							title: 'Test 2',
-							text: 'Before 8-bit brooklyn frado trust-fund ut. Chillwave capitalism placeat vegan. Anderson moon fin totally chowder original. The sunt dumpster dumpster voluptate chowder.',
-							published_at: new Date(2012, 5, 16),
-							image: '/images/test.jpg'
-						}, {
-							title: 'Test 3',
-							text: 'Clothesline esse sriracha gluten-free farm-to-table Pinterest. Carles street-art anim anime wes. Capitalism vegan gluten-free farm-to-table wayfarers gluten-free. Sint organic chowder street-art 8-bit anim of party. Narwhal of fresh twee. Viral vegan Anderson pony Anderson.',
-							published_at: new Date(2012, 3, 1),
-							image: '/images/test.jpg'
-						}]);
-					var articleList = articles.find({}, ['_id']).toArray(function(err, articleList) {
-						console.log(articleList);
-						feeds.save({
-							name: 'Smashing Magazine',
-							image_path: undefined,
-							feed_url: 'http://rss1.smashingmagazine.com/feed/',
-							articles: articleList
-						});
-					});
-				});
-			});
-			db.collection('users', function(err, users) {
-				//users.insert([]);
-				users.save({
-					email: 'jannes.meyer@gmail.com',
-					name: 'Jannes Meyer',
-					password: sha1('test'),
-					feeds: []
-				});
-				users.save({
-					email: 'fuhlig@stud.hs-bremen.de',
-					name: 'Florian Uhlig',
-					password: sha1('test'),
-					feeds: []
-				});
-				users.save({
-					email: 'magdalena.riecken@gmail.com',
-					name: 'Magdalena Riecken',
-					password: sha1('test'),
-					feeds: []
-				});
-				res.end('Success');
-			});
+			// Create users
+			db.users.insert([{
+				email: 'jannes.meyer@gmail.com',
+				name: 'Jannes Meyer',
+				password: sha1('test')
+			}, {
+				email: 'fuhlig@stud.hs-bremen.de',
+				name: 'Florian Uhlig',
+				password: sha1('test')
+			}, {
+				email: 'magdalena.riecken@gmail.com',
+				name: 'Magdalena Riecken',
+				password: sha1('test')
+			}]);
+			res.end('Success');
 		});
 	});
 
